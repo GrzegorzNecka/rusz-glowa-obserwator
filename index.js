8addcad7c3909b5e982e57043214aec793dd2f52
GrzegorzNecka <strzypior@gmail.com>
2021-04-11 15:40:56 +0200

    1 file modified

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
+import "./style.css";
 
-import './style.css';
-
-
-const appDiv = document.getElementById('app');
+const appDiv = document.getElementById("app");
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
 
+class DanePogodowe {
+  constructor() {}
 
-class DanePogodowe{
-  constructor(){
+  pobierzTemperature() {}
+  pobierzWilgotnosc() {}
+  pobierzCisnienie() {}
+  odczytZmiana() {
+    temp = pobierzTemperature();
+    wilgotnosc = pobierzWilgotnosc();
+    cisnienie = pobierzCisnienie();
 
+    warunkiBiezaceWyświetl.aktualizacja(temp, wilgotnosc, cisnienie);
+    statystykaWyświetl.aktualizacja(temp, wilgotnosc, cisnienie);
+    prognozaWyświetl.aktualizacja(temp, wilgotnosc, cisnienie);
   }
-
-pobierzTemperature()
-pobierzWilgotnosc()
-pobierzCisnienie()
-odczytZmiana()
 }
